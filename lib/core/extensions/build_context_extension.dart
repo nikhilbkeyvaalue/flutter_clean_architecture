8520e158f8a37bba253e1acf61720ac6ad236e39
Nikhil Biju <nikhil.b@keyvalue.systems>
2024-10-15 15:49:13 +0530

    refact: code refactoring

--- lib/core/extensions/build_context_extension.dart
+++ lib/core/extensions/build_context_extension.dart
@@ -20,3 +20,4 @@ extension BuildContextHelper on BuildContext {
 
   ResponsiveBreakpointsData get rsp => ResponsiveBreakpoints.of(this);
 }
+
--- lib/domain/movies/repositories/movies_repo.dart
+++ lib/domain/movies/repositories/movies_repo.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_clean_architecture/core/models/resource.dart';
-import 'package:flutter_clean_architecture/domain/entities/movie_entity.dart';
+import 'package:flutter_clean_architecture/domain/movies/entities/movie_entity.dart';
 
 abstract class MoviesRepo {
   Future<Resource<List<MovieEntity>>> getMovies();
